753403a0d8985581ce4fd34f89f15a6eae20b35f
Joaoms98 <jvm1800@gmail.com>
2025-12-08 22:50:01 -0300

    refactor: adjust city grid

--- frontend/src/components/CityGrid.tsx
+++ frontend/src/components/CityGrid.tsx
@@ -1,99 +1,26 @@
 import Building from "./Building";
-import Street from "./Street";
 
 const CityGrid = () => {
+  const cityRows = [
+    [0, 1, 2, 3],
+  ];
+
   return (
-    <div className="w-full max-w-5xl mx-auto p-4">
-      {/* Desktop Grid Layout */}
-      <div className="hidden md:grid gap-4">
-        {/* Row 1 */}
-        <div className="flex items-stretch gap-2">
-          <div className="flex-1">
-            <Building id="building-1" label="building" imageIndex={0} />
-          </div>
-          <Street orientation="vertical" />
-          <div className="flex-1">
-            <Building id="building-2" label="building" imageIndex={1} />
-          </div>
-          <Street orientation="vertical" />
-          <div className="flex-1">
-            <Building id="building-3" label="building" imageIndex={2} />
+    <div className="w-full max-w-6xl mx-auto p-4 space-y-6">
+      {cityRows.map((row, rowIndex) => (
+        <div key={rowIndex}>
+          <div className="grid grid-cols-4 gap-4">
+            {row.map((img, colIndex) => (
+              <Building
+                key={`b-${rowIndex}-${colIndex}`}
+                id={`building-${rowIndex}-${colIndex}`}
+                label="building"
+                imageIndex={img}
+              />
+            ))}
           </div>
         </div>
-
-        {/* Horizontal Street */}
-        <Street orientation="horizontal" />
-
-        {/* Row 2 */}
-        <div className="flex items-stretch gap-2">
-          <div className="flex-1">
-            <Building id="building-4" label="building" imageIndex={1} />
-          </div>
-          <Street orientation="vertical" />
-          <div className="flex-1">
-            <Building id="building-5" label="building" imageIndex={2} />
-          </div>
-          <Street orientation="vertical" />
-          <div className="flex-1">
-            <Building id="building-6" label="building" imageIndex={0} />
-          </div>
-        </div>
-
-        {/* Horizontal Street */}
-        <Street orientation="horizontal" />
-
-        {/* Row 3 */}
-        <div className="flex items-stretch gap-2">
-          <div className="flex-1">
-            <Building id="building-7" label="building" imageIndex={2} />
-          </div>
-          <Street orientation="vertical" />
-          <div className="flex-1">
-            <Building id="building-8" label="building" imageIndex={0} />
-          </div>
-          <Street orientation="vertical" />
-          <div className="flex-1">
-            <Building id="building-9" label="building" imageIndex={1} />
-          </div>
-        </div>
-      </div>
-
-      {/* Mobile Grid Layout */}
-      <div className="md:hidden grid grid-cols-1 gap-4">
-        <div className="grid grid-cols-2 gap-2">
-          <Building id="building-1" label="building" imageIndex={0} />
-          <Building id="building-2" label="building" imageIndex={1} />
-        </div>
-        
-        <Street orientation="horizontal" />
-        
-        <div className="grid grid-cols-2 gap-2">
-          <Building id="building-3" label="building" imageIndex={2} />
-          <Building id="building-4" label="building" imageIndex={1} />
-        </div>
-        
-        <Street orientation="horizontal" />
-        
-        <div className="grid grid-cols-2 gap-2">
-          <Building id="building-5" label="building" imageIndex={2} />
-          <Building id="building-6" label="building" imageIndex={0} />
-        </div>
-        
-        <Street orientation="horizontal" />
-        
-        <div className="grid grid-cols-2 gap-2">
-          <Building id="building-7" label="building" imageIndex={2} />
-          <Building id="building-8" label="building" imageIndex={0} />
-        </div>
-        
-        <Street orientation="horizontal" />
-        
-        <div className="flex justify-center">
-          <div className="w-1/2">
-            <Building id="building-9" label="building" imageIndex={1} />
-          </div>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
